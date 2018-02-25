a03c386af9f29372e193520b462cfe3d5a75db1f
frankamente <fran29400@gmail.com>
2018-02-25 14:13:19 +0100

    BubbleSort Program

---
+++ src/main/java/BubbleSort.java
@@ -0,0 +1,34 @@
+public class BubbleSort {
+
+
+    private int[] messyList;
+
+    public BubbleSort(int[] messyList){
+        this.messyList = messyList;
+    }
+
+    public static void main(String[] args) {
+
+        int[] messyList = new int[]{6,5,3,1,8,7,2,4};
+        BubbleSort bubbleSort = new BubbleSort(messyList);
+
+        int[] sortedList = bubbleSort.sort();
+        for (int number : sortedList) {
+            System.out.print(number+";");
+        }
+    }
+
+    private int[] sort(){
+        int[] sortedList = messyList.clone();
+        for (int i=sortedList.length; i>1; i--){
+            for (int j=0; j<(i-1); j++){
+                if (sortedList[j+1]<sortedList[j]){
+                    int aux = sortedList[j];
+                    sortedList[j] = sortedList[j+1];
+                    sortedList[j+1] = aux;
+                }
+            }
+        }
+        return sortedList;
+    }
+}
